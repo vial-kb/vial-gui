b9fc1f6ceeaba3c96e9c0bc4f87df12516789a05
arnstadm <arnstad@gmail.com>
2021-01-11 14:01:12 +0100

    added norwegian keymap

---
+++ src/main/python/keymap/norwegian.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+keymap = {
+    "KC_GRAVE": "|\n§",
+    "KC_2": '"\n2',
+    "KC_3": "#\n3",
+    "KC_4": "¤\n4",
+    "KC_6": "&\n6",
+    "KC_7": "/\n7",
+    "KC_8": "(\n8",
+    "KC_9": ")\n9",
+    "KC_0": "=\n0",
+    "KC_MINS": "+\n?",
+    "KC_EQL": "\\\n`",
+    "KC_LBRACKET": "Å",
+    "KC_RBRACKET": "¨\n^",
+    "KC_SCOLON": "Ø",
+    "KC_QUOTE": "Æ",
+    "KC_NUHS": "'\n*",
+    "KC_NUBS": "<\n>",
+    "KC_COMMA": ";\n,",
+    "KC_DOT": ":\n.",
+    "KC_SLASH": "_,\n-"
+}
