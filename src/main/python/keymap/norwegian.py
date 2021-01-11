980744eba523e9ae18c162666f1d3f466ad5d706
arnstadm <arnstad@gmail.com>
2021-01-11 15:50:00 +0100

    corrected keycodes

--- src/main/python/keymap/norwegian.py
+++ src/main/python/keymap/norwegian.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 keymap = {
-    "KC_GRAVE": "|\n§",
+    "KC_GRAVE": "§\n|",
     "KC_2": '"\n2',
     "KC_3": "#\n3",
     "KC_4": "¤\n4",
@@ -10,15 +10,15 @@ keymap = {
     "KC_8": "(\n8",
     "KC_9": ")\n9",
     "KC_0": "=\n0",
-    "KC_MINS": "+\n?",
-    "KC_EQL": "\\\n`",
+    "KC_MINUS": "?\n+",
+    "KC_EQUAL": "`\n\\",
     "KC_LBRACKET": "Å",
-    "KC_RBRACKET": "¨\n^",
+    "KC_RBRACKET": "^\n¨",
     "KC_SCOLON": "Ø",
     "KC_QUOTE": "Æ",
-    "KC_NUHS": "'\n*",
-    "KC_NUBS": "<\n>",
+    "KC_NONUS_HASH": "*\n'",
+    "KC_NONUS_BSLASH": ">\n<",
     "KC_COMMA": ";\n,",
     "KC_DOT": ":\n.",
-    "KC_SLASH": "_,\n-"
+    "KC_SLASH": "-\n_"
 }
