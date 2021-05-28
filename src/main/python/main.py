144907d477d687d111324c9044a7716e0db960e9
Juan Pablo Kutianski <jkutianski@gmail.com>
2021-05-28 15:13:06 -0300

    Move VIAL-APP isRunning() & added VIAL-LINUX-RECORDER isRunning()

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,10 +1,3 @@
-import sys
-
-from single_application import QtSingleApplication
-
-app = QtSingleApplication('EA48-A684-BD42-588B-2EA1-2E08-12BD-2418', sys.argv)
-if app.isRunning(): sys.exit(0)
-
 # SPDX-License-Identifier: GPL-2.0-or-later
 import ssl
 import certifi
@@ -19,6 +12,7 @@ from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import pyqtSignal
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
 
+import sys
 
 from main_window import MainWindow
 from constants import WINDOW_WIDTH, WINDOW_HEIGHT
@@ -26,6 +20,9 @@ from constants import WINDOW_WIDTH, WINDOW_HEIGHT
 
 # http://timlehr.com/python-exception-hooks-with-qt-message-box/
 from util import init_logger
+
+#Single instance app
+from single_application import QtSingleApplication
 
 
 def show_exception_box(log_msg):
@@ -63,10 +60,16 @@ class UncaughtHook(QtCore.QObject):
 
 if __name__ == '__main__':
     if len(sys.argv) == 2 and sys.argv[1] == "--linux-recorder":
+        app = QtSingleApplication('3A07-4923-A4C0-45CD-FAE9-F3D5-0177-52B9', sys.argv) #VIAL-LINUX-RECORDER
+        if app.isRunning(): sys.exit(0)
+
         from linux_keystroke_recorder import linux_keystroke_recorder
 
         linux_keystroke_recorder()
     else:
+        app = QtSingleApplication('EA48-A684-BD42-588B-2EA1-2E08-12BD-2418', sys.argv) #VIAL-APP
+        if app.isRunning(): sys.exit(0)
+
         appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
         init_logger()
         qt_exception_hook = UncaughtHook()
