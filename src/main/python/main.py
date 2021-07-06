5e5723722e68cb38c64b2877fd8117ccd52d6e69
Ilya Zhuravlev <whatever@xyz.is>
2021-07-06 02:28:44 -0400

    Remove fbs, switch to pyinstaller

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -10,7 +10,7 @@ import traceback
 
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import pyqtSignal
-from fbs_runtime.application_context.PyQt5 import ApplicationContext
+from PyQt5.QtWidgets import QApplication
 
 import sys
 
@@ -61,11 +61,10 @@ if __name__ == '__main__':
 
         linux_keystroke_recorder()
     else:
-        appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
+        app = QApplication(sys.argv)
         init_logger()
         qt_exception_hook = UncaughtHook()
         window = MainWindow(appctxt)
         window.resize(WINDOW_WIDTH, WINDOW_HEIGHT)
         window.show()
-        exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
-        sys.exit(exit_code)
+        sys.exit(app.exec_())
