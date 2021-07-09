6bfd4bc3ec11adf6fd892964a46d4d8b06f28400
Ilya Zhuravlev <whatever@xyz.is>
2021-07-09 17:54:15 -0400

    vialrgb: add all supported effects

--- src/main/python/keyboard_comm.py
+++ src/main/python/keyboard_comm.py
@@ -397,7 +397,8 @@ class Keyboard:
             self.rgb_supported_effects = {0}
             max_effect = 0
             while max_effect < 0xFFFF:
-                data = self.usb_send(self.dev, struct.pack("BB", CMD_VIA_LIGHTING_GET_VALUE, VIALRGB_GET_SUPPORTED))[2:]
+                data = self.usb_send(self.dev, struct.pack("<BBH", CMD_VIA_LIGHTING_GET_VALUE, VIALRGB_GET_SUPPORTED,
+                                                           max_effect))[2:]
                 for x in range(0, len(data), 2):
                     value = int.from_bytes(data[x:x+2], byteorder="little")
                     if value != 0xFFFF:
