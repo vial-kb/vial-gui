9f5b5db8e4de80b2d7c965a11c494188859a3f0a
NCKiser <noahkiser95mygc@gmail.com>
2021-11-07 01:50:50 -0500

    Sequestered Import and Export to keys ctrl I and O

--- src/main/python/textbox_window.py
+++ src/main/python/textbox_window.py
@@ -3,7 +3,7 @@ import time
 
 from PyQt5.QtCore import QCoreApplication, Qt
 from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QDialog, QDialogButtonBox, \
-    QPlainTextEdit, QToolButton, QFileDialog
+    QPlainTextEdit, QToolButton, QFileDialog, QWidget
 
 from util import tr
 
@@ -26,20 +26,15 @@ class TextboxWindow(QDialog):
         self.macrotext.setPlainText(text)
         self.macrotext.selectAll()
 
-        self.btn_save_exit = QToolButton()
-        self.btn_save_exit.setText(tr("TextboxWindow", "Save"))
-        self.btn_save_exit.setToolButtonStyle(Qt.ToolButtonTextOnly)
-        self.btn_save_exit.clicked.connect(self.on_save_exit)
+        self.btn_apply = QToolButton()
+        self.btn_apply.setText(tr("TextboxWindow", "Apply"))
+        self.btn_apply.setToolButtonStyle(Qt.ToolButtonTextOnly)
+        self.btn_apply.clicked.connect(self.on_apply)
 
         self.btn_cancel = QToolButton()
         self.btn_cancel.setText(tr("TextboxWindow", "Cancel"))
         self.btn_cancel.setToolButtonStyle(Qt.ToolButtonTextOnly)
         self.btn_cancel.clicked.connect(self.on_cancel)
-
-        self.btn_select_all = QToolButton()
-        self.btn_select_all.setText(tr("TextboxWindow", "Select All"))
-        self.btn_select_all.setToolButtonStyle(Qt.ToolButtonTextOnly)
-        self.btn_select_all.clicked.connect(self.on_select_all)
 
         self.btn_copy = QToolButton()
         self.btn_copy.setText(tr("TextboxWindow", "Copy"))
@@ -51,55 +46,38 @@ class TextboxWindow(QDialog):
         self.btn_paste.setToolButtonStyle(Qt.ToolButtonTextOnly)
         self.btn_paste.clicked.connect(self.on_paste)
 
-        self.btn_export = QToolButton()
-        self.btn_export.setText(tr("TextboxWindowr", "Export"))
-        self.btn_export.setToolButtonStyle(Qt.ToolButtonTextOnly)
-        self.btn_export.clicked.connect(self.on_export)
-
-        self.btn_import = QToolButton()
-        self.btn_import.setText(tr("TextboxWindow", "Import"))
-        self.btn_import.setToolButtonStyle(Qt.ToolButtonTextOnly)
-        self.btn_import.clicked.connect(self.on_import)
-
-        top_buttons = QHBoxLayout()
-        top_buttons.addWidget(self.btn_import)
-        top_buttons.addWidget(self.btn_export)
-        top_buttons.addStretch()
-        top_buttons.addWidget(self.btn_select_all)
-
         bottom_buttons = QHBoxLayout()
+        bottom_buttons.setContentsMargins(0, 0, 0, 0)
         bottom_buttons.addWidget(self.btn_copy)
         bottom_buttons.addWidget(self.btn_paste)
+        bottom_buttons.addSpacing(15)
         bottom_buttons.addStretch()
-        bottom_buttons.addWidget(self.btn_save_exit)
+        bottom_buttons.addWidget(self.btn_apply)
         bottom_buttons.addWidget(self.btn_cancel)
 
-        vbox.addLayout(top_buttons)
+        self.bottom_widget = QWidget()
+        self.bottom_widget.setLayout(bottom_buttons)
+
         vbox.addWidget(self.macrotext, stretch=1)
-        vbox.addLayout(bottom_buttons)
+        vbox.addWidget(self.bottom_widget)
 
         self.setLayout(vbox)
         self.setWindowFlags(self.windowFlags())
 
-    def on_save_exit(self):
+    def on_apply(self):
         self.accept()
-        return
 
     def on_cancel(self):
         self.reject()
-        return
 
     def on_select_all(self):
         self.macrotext.selectAll()
-        return
 
     def on_copy(self):
         self.macrotext.copy()
-        return
 
     def on_paste(self):
         self.macrotext.paste()
-        return
 
     def on_export(self):
         dialog = QFileDialog()
@@ -110,7 +88,6 @@ class TextboxWindow(QDialog):
         if dialog.exec_() == QDialog.Accepted:
             with open(dialog.selectedFiles()[0], "wb") as outf:
                 outf.write(self.macrotext.toPlainText().encode(self.encoding))
-        return
 
     def on_import(self):
         dialog = QFileDialog()
@@ -121,7 +98,19 @@ class TextboxWindow(QDialog):
         if dialog.exec_() == QDialog.Accepted:
             with open(dialog.selectedFiles()[0], "rb") as inf:
                 self.macrotext.setPlainText(inf.read().decode(self.encoding))
-        return
 
     def getText(self):
         return self.macrotext.toPlainText()
+
+    def keyPressEvent(self, ev):
+        if ev.key() == Qt.Key_Escape:
+            self.reject()
+
+        if ev.key() == Qt.Key_I:
+            self.on_import()
+
+        if ev.key() == Qt.Key_O:
+            self.on_export()
+
+        if ev.key() == Qt.Key_S:
+            self.on_apply()
