4abae2f820b36c3c78ea9730ce8b107305dadaba
Ilya Zhuravlev <whatever@xyz.is>
2022-01-02 13:53:14 -0500

    key_override: implement protocol

--- src/main/python/protocol/key_override.py
+++ src/main/python/protocol/key_override.py
@@ -1,14 +1,68 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
+import struct
+
 from protocol.base_protocol import BaseProtocol
-from protocol.constants import DYNAMIC_VIAL_KEY_OVERRIDE_GET
+from protocol.constants import DYNAMIC_VIAL_KEY_OVERRIDE_GET, CMD_VIA_VIAL_PREFIX, CMD_VIAL_DYNAMIC_ENTRY_OP, \
+    DYNAMIC_VIAL_KEY_OVERRIDE_SET
 
 
-class KeyOverride:
-    pass
+class KeyOverrideOptions:
+
+    def __init__(self, data):
+        self.activation_trigger_down = bool(data & (1 << 0))
+        self.activation_required_mod_down = bool(data & (1 << 1))
+        self.activation_negative_mod_up = bool(data & (1 << 2))
+        self.one_mod = bool(data & (1 << 3))
+        self.no_reregister_trigger = bool(data & (1 << 4))
+        self.no_unregister_on_other_key_down = bool(data & (1 << 5))
+        self.enabled = bool(data & (1 << 7))
+
+    def serialize(self):
+        return (int(self.activation_trigger_down) << 0) \
+            | (int(self.activation_required_mod_down) << 1) \
+            | (int(self.activation_negative_mod_up) << 2) \
+            | (int(self.one_mod) << 3) \
+            | (int(self.no_reregister_trigger) << 4) \
+            | (int(self.no_unregister_on_other_key_down) << 5) \
+            | (int(self.enabled) << 7)
+
+    def __repr__(self):
+        return "KeyOverrideOptions<{}>".format(self.serialize())
+
+
+class KeyOverrideEntry:
+
+    def __init__(self, args):
+        self.trigger, self.replacement, self.layers, self.trigger_mods, self.negative_mod_mask, \
+            self.suppressed_mods, opt = args
+        self.options = KeyOverrideOptions(opt)
+
+    def serialize(self):
+        """ Serializes into a vial_key_override_entry_t object """
+        return struct.pack("<HHHBBBB", self.trigger, self.replacement, self.layers, self.trigger_mods,
+                           self.negative_mod_mask, self.suppressed_mods, self.options.serialize())
+
+    def __repr__(self):
+        return "KeyOverride<trigger={} replacement={} layers={} trigger_mods={} negative_mod_mask={} " \
+               "suppresed_mods={} options={}>".format(self.trigger, self.replacement, self.layers, self.trigger_mods,
+                                                      self.negative_mod_mask, self.suppressed_mods, self.options)
+
+    def __eq__(self, other):
+        return self.serialize() == other.serialize()
 
 
 class ProtocolKeyOverride(BaseProtocol):
 
     def reload_key_override(self):
-        self.key_override_entries = self._retrieve_dynamic_entries(DYNAMIC_VIAL_KEY_OVERRIDE_GET,
-                                                                   self.key_override_count, "<HHHBBBB")
+        entries = self._retrieve_dynamic_entries(DYNAMIC_VIAL_KEY_OVERRIDE_GET,
+                                                 self.key_override_count, "<HHHBBBB")
+        self.key_override_entries = [KeyOverrideEntry(e) for e in entries]
+
+    def key_override_get(self, idx):
+        return self.key_override_entries[idx]
+
+    def key_override_set(self, idx, entry):
+        if entry != self.key_override_entries[idx]:
+            self.key_override_entries[idx] = entry
+            self.usb_send(self.dev, struct.pack("BBBB", CMD_VIA_VIAL_PREFIX, CMD_VIAL_DYNAMIC_ENTRY_OP,
+                                                DYNAMIC_VIAL_KEY_OVERRIDE_SET, idx) + entry.serialize())
