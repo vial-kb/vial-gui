76a5d7d254fe1b83e93d092d65e833165a6a7ecd
Fernando Pietchaki <6844712+pietchaki@users.noreply.github.com>
2022-12-13 04:22:15 -0300

    added NUHS keycode

--- src/main/python/keymap/brazilian.py
+++ src/main/python/keymap/brazilian.py
@@ -8,9 +8,12 @@ keymap = {
     "KC_LBRACKET": "`\n´",
     "KC_RBRACKET": "{\n[",
     "KC_BSLASH": "}\n]",
+    "KC_NONUS_HASH": "}\n]",
     "KC_NONUS_BSLASH": "|\n\\",
     "KC_RO": "?\n/",
     "KC_6": "¨\n6",
+    # On the ABNT2 the keypad comma and the keypad dot scancodes are switched
+    # (presumably because in Brazil comma is used as the decimal separator)
     "KC_KP_COMMA": ".",
     "KC_KP_DOT": ","
 }
