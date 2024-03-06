396052ed9337ab68e28a85df75e29b0c03d006d0
Var <120035196+itsvar8@users.noreply.github.com>
2024-03-06 11:25:50 +0100

    Italian added

---
+++ src/main/python/keymap/italian.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+keymap = {
+    "KC_GRAVE": "|\n\\",
+    "KC_1": "!\n1",
+    "KC_2": '"\n2',
+    "KC_3": "£\n3",
+    "KC_6": "&\n6",
+    "KC_7": "/\n7",
+    "KC_8": "(\n8",
+    "KC_9": ")\n9",
+    "KC_0": "=\n0",
+    "KC_MINUS": "´\n?",
+    "KC_EQUAL": "^\nì",
+    "KC_E": "E\n   €",
+    "KC_LBRACKET": "é\nè [",
+    "KC_RBRACKET": "*\n+ ]",
+    "KC_BSLASH": "ù\n\§",
+    "KC_SCOLON": "ç\nò @",
+    "KC_QUOTE": "º\nà #",
+    "KC_NONUS_HASH": "ù\n\§",
+    "KC_NONUS_BSLASH": ">\n<",
+    "KC_COMMA": ";\n,",
+    "KC_DOT": ":\n.",
+    "KC_SLASH": "_\n-",
+}
