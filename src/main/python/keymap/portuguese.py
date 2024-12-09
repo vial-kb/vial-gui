231be38b74e1af43be54d0c75fa7cd7f86d52f37
lowkey <ana.margarida.silva@protonmail.com>
2024-12-09 16:58:47 +0000

    keymap: added portuguese qwerty layout

---
+++ src/main/python/keymap/portuguese.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+keymap = {
+    "KC_COMMA": ";\n,",
+    "KC_DOT": ":\n.",
+    "KC_SLASH": "_\n-",
+    "KC_SCOLON": "Ç",
+    "KC_QUOTE": "ª\nº",
+    "KC_LBRACKET": "*\n+ ¨",
+    "KC_RBRACKET": "`\n´",
+    "KC_BSLASH": "^\n~",
+    "KC_NONUS_HASH": "^\n~",
+    "KC_NONUS_BSLASH": ">\n<",
+    "KC_GRAVE": "|\n\\",
+    "KC_2": "\"\n2 @",
+    "KC_3": "#\n3 £",
+    "KC_4": "$\n4 §",
+    "KC_7": "/\n7 {",
+    "KC_8": "(\n8 [",
+    "KC_9": ")\n9 ]",
+    "KC_0": "=\n0 }",
+    "KC_E": "E\n €",
+    "KC_MINUS": "'\n?",
+    "KC_EQUAL": "»\n«"
+}
+
