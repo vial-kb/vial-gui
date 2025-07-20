73dde47d7c06c1bc70da2acc76cdbcb5202fbc2f
bazsalazas <77466081+bazsalazas@users.noreply.github.com>
2025-07-20 07:45:26 +0200

    Add Colemak Mod-DH layouts (#339)
    * Add colemak DH mod layouts
    
    * Add colemak DH mod layout modules

---
+++ src/main/python/keymap/colemak_dh_matrix.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+keymap = {
+    "KC_E": "F",
+    "KC_R": "P",
+    "KC_T": "B",
+    "KC_Y": "J",
+    "KC_U": "L",
+    "KC_I": "U",
+    "KC_O": "Y",
+    "KC_P": ";",
+    "KC_S": "R",
+    "KC_D": "S",
+    "KC_F": "T",
+    "KC_H": "M",
+    "KC_J": "N",
+    "KC_K": "E",
+    "KC_L": "I",
+    "KC_SCOLON": "O",
+    "KC_V": "D",
+    "KC_B": "V",
+    "KC_N": "K",
+    "KC_M": "H",
+}
